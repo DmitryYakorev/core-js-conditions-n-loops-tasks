6f5a32c86b0b97697704fcb8c7dc0253bc805db2
Dmitry Yakorev <urakend@mail.ru>
2024-01-09 16:45:35 +0300

    feat:new1_up1

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -208,7 +208,7 @@ function convertNumberToString(numberStr) {
  *  '0123210'   => true
  *  'qweqwe'    => false
  */
-function isPalindrome(str) {
+function isPalindrome(/* str */) {
   throw new Error('Not implemented');
 }
 
